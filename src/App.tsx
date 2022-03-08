619d9d0a6cbded2cecbd77c6a025034f35ff3696
Tomasz Watroba <w.t.tomasz@gmail.com>
2022-03-08 22:51:43 +0100

    basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import './App.scss';
+import logo from './img/logo.svg';
 
 function App() {
+  // useEffect(() => {
+
+  // }, []);
   return (
     <div className="App">
-      hello
+      <header>
+        <img src={logo} alt="logo" />
+      </header>
+      <main>
+
+      </main>
+      <footer>
+        <p>STOPKA GAME</p>
+      </footer>
     </div>
   );
 }
